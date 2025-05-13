e7288d0b39897adf735ad8580d62399af595bac9
Rithwik Anand <ranand@planview.com>
2025-05-13 11:14:36 +0530

    Simplify the hash function

--- libs/shared/utils/src/get-port-from-name.ts
+++ libs/shared/utils/src/get-port-from-name.ts
@@ -1,8 +1,7 @@
 export const getPortFromName = (name: string, base = 3000) => {
     let hash = 0;
     for (let i = 0; i < name.length; i++) {
-        hash = (hash << 5) - hash + name.charCodeAt(i);
-        hash |= 0;
+        hash = (hash * 31 + name.charCodeAt(i)) | 0;
     }
     return base + (Math.abs(hash) % 1000);
 };

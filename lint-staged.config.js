ee427c2f4f8e3866c844b0f61726f142ff077460
Rithwik Anand <ranand@planview.com>
2025-05-24 14:33:28 +0530

    Add syncpack checks for package.json changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 export default {
     '*.{js,mjs,cjs,ts,jsx,tsx}': [() => 'pnpm -F "...[HEAD]..." lint --fix', 'prettier --write'],
+    '**/package.json': () => ['syncpack format', 'syncpack fix'],
 };

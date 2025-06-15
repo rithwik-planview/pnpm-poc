643ffcf12e9173132fee28d173a607c6982b524d
Rithwik Anand <ranand@planview.com>
2025-06-15 21:46:01 +0530

    Fix apps.demo-umd failing on local environments

--- apps/demo-umd/rsbuild.config.ts
+++ apps/demo-umd/rsbuild.config.ts
@@ -12,13 +12,36 @@ export default defineConfig({
             {
                 tag: 'script',
                 attrs: {
-                    src: '../../@unity/core.shell/umd/index.js',
+                    src: '@unity/core.shell/umd/index.js',
                 },
             },
         ],
     },
     output: {
         assetPrefix: 'auto',
+        copy: [
+            {
+                from: '../../libs/core/shell/dist/umd/',
+                to: '@unity/core.shell/umd/',
+                globOptions: {
+                    ignore: ['*.LICENSE.txt'],
+                },
+            },
+            {
+                from: '../../libs/features/feature-1/dist/mf/',
+                to: '@unity/features.feature-1/mf/',
+                globOptions: {
+                    ignore: ['*.LICENSE.txt'],
+                },
+            },
+            {
+                from: '../../libs/features/feature-2/dist/mf/',
+                to: '@unity/features.feature-2/mf/',
+                globOptions: {
+                    ignore: ['*.LICENSE.txt'],
+                },
+            },
+        ],
     },
     server: {
         port: getPortFromName(name),
